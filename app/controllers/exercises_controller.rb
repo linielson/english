824ba279dc2295173edf768a7f9ef71ecf024259
Linielson Rosa <linielsonrosa@gmail.com>
2012-09-15 11:37:47 -0300

    Verify the correct answer in the exercise

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,29 +1,24 @@
 # encoding: UTF-8
 class ExercisesController < ApplicationController
-  # GET /exercises
-  # GET /exercises.json
+  
   def index
     @exercises = Exercise.all
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html 
       format.json { render json: @exercises }
     end
   end
 
-  # GET /exercises/1
-  # GET /exercises/1.json
   def show
     @exercise = Exercise.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html 
       format.json { render json: @exercise }
     end
   end
 
-  # GET /exercises/new
-  # GET /exercises/new.json
   def new
     @exercise = Exercise.new
     @exercise.language_answer = 'English'
@@ -35,13 +30,6 @@ class ExercisesController < ApplicationController
     end
   end
 
-  # GET /exercises/1/edit
-  def edit
-    @exercise = Exercise.find(params[:id])
-  end
-
-  # POST /exercises
-  # POST /exercises.json
   def create
     @exercise = Exercise.new(params[:exercise])
 
@@ -56,31 +44,4 @@ class ExercisesController < ApplicationController
     end
   end
 
-  # PUT /exercises/1
-  # PUT /exercises/1.json
-  def update
-    @exercise = Exercise.find(params[:id])
-
-    respond_to do |format|
-      if @exercise.update_attributes(params[:exercise])
-        format.html { redirect_to @exercise, notice: 'Exercise was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @exercise.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /exercises/1
-  # DELETE /exercises/1.json
-  def destroy
-    @exercise = Exercise.find(params[:id])
-    @exercise.destroy
-
-    respond_to do |format|
-      format.html { redirect_to exercises_url }
-      format.json { head :no_content }
-    end
-  end
 end
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,29 @@
+# Encoding:UTF-8
 class Exercise < ActiveRecord::Base
   belongs_to :expression
+  
+  before_save :correct_question  
+  
+  def question
+    if language_answer == "Português"
+      expression.english
+    else
+      expression.portuguese
+    end
+  end
+  
+private
+
+  def correct_question
+    if language_answer == "Português"
+      self.correct_answer = self.answer == self.expression.portuguese
+    else
+      self.correct_answer = self.answer == self.expression.english
+    end
+
+    true # Ficamos por corrigir essa questão. Quando retorna false no
+         # before_save ele não continua. Visto que só queriamos carregar o
+         # campo "correct_answer".
+  end
+  
 end
